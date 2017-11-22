4456b8f1794051cc3c47a3eb07d94105cdc6f635
BredeJoh <brede_96@hotmail.com>
2017-11-22 22:32:15 +0100

    parent child transforms
    transforms can now have parent and children, but only position is updated correctly so far

--- Vism oblig 4/Common/transform.js
+++ Vism oblig 4/Common/transform.js
@@ -2,11 +2,20 @@
 
 function Transform(position, rotation, scale)
 {
+    // local space -position, rotation and scale
+    this.localPosition = position;
+    this.localRotation = rotation;
+    this.localScale = scale;
+
+    // world space - position, rotation and scale
     this.position = position;
     this.rotation = rotation;
     this.scale = scale;
+
     this.matrix = [];
     this.change = true;
+    this.parent; // this is a transform
+    this.children = []; // this is also transforms
 }
 
 Transform.prototype.getMatrix = function()
@@ -18,6 +27,7 @@ Transform.prototype.getMatrix = function()
 
 Transform.prototype.updateMatrix = function()
 {
+    //this.updateTransform();
     this.change = false;
     var mat = mat4();
     mat = mult(mat, translate(this.position[0], this.position[1], this.position[2]));
@@ -32,8 +42,9 @@ Transform.prototype.setPosition = function(position)
 {
     if (position.length == 3)
     {
-        this.position = position;
+        this.localPosition = position;
         this.change = true;
+        this.updateTransform();
     }
 }
 
@@ -41,16 +52,18 @@ Transform.prototype.setRotation = function (rotation)
 {
     if (rotation.length == 3)
     {
-        this.rotation = rotation;
+        this.localRotation = rotation;
         this.change = true;
+        this.updateTransform();
     }
 }
 
 Transform.prototype.setScale = function (scale) {
     if (scale.length == 3)
     {
-        this.scale = scale;
+        this.localScale = scale;
         this.change = true;
+        this.updateTransform();
     }
 }
 
@@ -59,9 +72,10 @@ Transform.prototype.translate = function(by)
     if(by.length == 3)
     {
         for(var i=0; i < 3; i++)
-            this.position[i] += by[i];
+            this.localPosition[i] += by[i];
 
-        this.change=true;
+        this.change = true;
+        this.updateTransform();
     }
 }
 
@@ -70,9 +84,10 @@ Transform.prototype.rotate = function(by)
     if(by.length == 3)
     {
         for(var i=0; i < 3; i++)
-            this.rotation[i] += by[i];
+            this.localRotation[i] += by[i];
 
-        this.change=true;
+        this.change = true;
+        this.updateTransform();
     }
 }
 
@@ -81,8 +96,69 @@ Transform.prototype.scalar = function(by)
     if(by.length == 3)
     {
         for(var i=0; i < 3; i++)
-            this.scale[i] += by[i];
+            this.localScale[i] += by[i];
 
-        this.change=true;
+        this.change = true;
+        this.updateTransform();
     }
+}
+// only for internal use !!!!! as used below
+Transform.prototype.setChild = function(child)
+{
+    // adds child, sets this as childs parent, and updates its transform
+    this.children.push(child); 
+    child.updateTransform();
+}
+
+Transform.prototype.setParent = function(parent)
+{
+    // sets parent to input, set this to parents children and updates transform
+    this.parent = parent; 
+    parent.setChild(this)
+    this.updateTransform();
+}
+
+Transform.prototype.getWorldPosition = function()
+{   //recursive function returns all parent positions in hierarchy and adds them
+    if (this.parent)
+        return add(this.localPosition, this.parent.getWorldPosition());
+    else
+        return this.localPosition;
+}
+
+Transform.prototype.getWorldRotation = function ()
+{   //recursive function returns all parent rotations in hierarchy and adds them
+    if (this.parent)
+        return add(this.localRotation, this.parent.getWorldRotation());
+    else
+        return this.localRotation;
+}
+
+Transform.prototype.getWorldScale = function ()
+{   //recursive function returns all parent scales in hierarchy and adds them
+    if (this.parent)
+        return add(this.localScale, this.parent.getWorldScale());
+    else
+        return this.localScale;
+}
+
+Transform.prototype.updateTransform = function ()
+{
+    if (this.parent) {
+        this.position = this.getWorldPosition();
+        this.rotation = this.getWorldRotation();
+        this.scale = this.getWorldScale();
+    }
+    else
+    {
+        this.position = this.localPosition;
+        this.rotation = this.localRotation;
+        this.scale = this.localScale;
+    }
+    if (this.children.length != 0) {
+        for (var i = 0; i < this.children.length; i++) {
+            this.children[i].updateTransform();
+        }
+    }
+    this.change = true;
 }
